38f8a1fed1456566b4baf5f973095035b218de2f
Borislav Ivanov <slambeca@abv.bg>
2024-01-29 13:48:18 +0200

    Add files via upload

--- 6. Workshop 1/Petstagram/Petstagram/photos/admin.py
+++ 6. Workshop 1/Petstagram/Petstagram/photos/admin.py
@@ -5,4 +5,14 @@ from Petstagram.photos.models import PetPhoto
 
 @admin.register(PetPhoto)
 class PetPhotoAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("pk", "location", "tagged_pets", "short_description")
+
+    # @staticmethod
+    # def short_description(self, obj):
+    #     return obj.description[:5]
+
+    def tagged_pets(self, obj):
+        return ", ".join(pet.name for pet in obj.pets.all())
+
+    def short_description(self, obj):
+        return obj.description[:6]
