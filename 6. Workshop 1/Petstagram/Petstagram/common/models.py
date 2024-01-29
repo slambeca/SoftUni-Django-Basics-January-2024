22b12ead65f3c3c5648c25ce05ddcdbd0314c873
Borislav Ivanov <slambeca@abv.bg>
2024-01-29 13:47:48 +0200

    Add files via upload

--- 6. Workshop 1/Petstagram/Petstagram/common/models.py
+++ 6. Workshop 1/Petstagram/Petstagram/common/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+from Petstagram.photos.models import PetPhoto
+
+
+class PhotoComment(models.Model):
+    MAX_TEXT_LENGTH = 300
+
+    text = models.TextField(
+        max_length=MAX_TEXT_LENGTH,
+        null=False,
+        blank=False,
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    modified_at = models.DateTimeField(
+        auto_now=True,
+    )
+
+    pet_photo = models.ForeignKey(
+        PetPhoto, on_delete=models.DO_NOTHING
+    )
+
+    # user = models.ForeignKey(User)
+
+
+class PhotoLike(models.Model):
+    pet_photo = models.ForeignKey(
+        PetPhoto, on_delete=models.DO_NOTHING
+    )
+
+    # user = models.ForeignKey(User)
