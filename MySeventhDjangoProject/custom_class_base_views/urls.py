2257d361b5c8cfbcf8acc4f123bd272c9ecc3536
Borislav Ivanov <slambeca@abv.bg>
2024-02-12 20:22:49 +0200

    Delete MySeventhDjangoProject directory

--- MySeventhDjangoProject/custom_class_base_views/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from MySeventhDjangoProject.custom_class_base_views.views import index
-from MySeventhDjangoProject.web.views import IndexView
-
-urlpatterns = (
-    path("", index, name="ccbc_index"),
-    path("cbv/", IndexView.as_view(), name="ccbc_cbv_index"),
-)
--- MySeventhDjangoProject/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", include('MySeventhDjangoProject.web.urls')),
-    path("custom/", include('MySeventhDjangoProject.custom_class_base_views.urls')),
-]
